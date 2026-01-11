6abdfa4d5386fed51b8ba589e461dd175daf5eca
R Akhrom Darmawan <rahmat.darmawan16@student.uisi.ac.id>
2026-01-11 09:24:27 +0700

    update

--- config/database.go
+++ config/database.go
@@ -16,17 +16,18 @@ func InitDB() {
 	if err := godotenv.Load(); err != nil {
 		fmt.Println("Warning: .env not found, using system env")
 	}
+	dsn := os.Getenv("DATABASE_URL")
 
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	pass := os.Getenv("DB_PASSWORD")
-	name := os.Getenv("DB_NAME")
+	// host := os.Getenv("DB_HOST")
+	// port := os.Getenv("DB_PORT")
+	// user := os.Getenv("DB_USER")
+	// pass := os.Getenv("DB_PASSWORD")
+	// name := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, pass, name,
-	)
+	// dsn := fmt.Sprintf(
+	// 	"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+	// 	host, port, user, pass, name,
+	// )
 
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
